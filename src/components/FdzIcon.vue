bcb7de14da30f4caf930c589358b023f85b73594
Scott Turner <sturner1979@hotmail.com>
2020-07-27 09:36:23 +0100

    Added icons to funds view

--- src/components/FdzIcon.vue
+++ src/components/FdzIcon.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="fdz-icon">
-    icon
+    <i :class="iconClass"></i>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-
+  props: ['iconClass']
 }
 </script>
 
 <style scoped lang="scss">
 @import '@/styles/fdz-styles';
 
+$iconColour: $colourWhite;
+$bg: $colourMineShaft;
+
+.fdz-icon {
+  background: $bg;
+  border-radius: 4px;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  i {
+    color: $iconColour;
+  }
+
+  &:hover {
+    background: lighten($bg, 20%);
+  }
+
+}
 </style>
--- src/views/Funds.vue
+++ src/views/Funds.vue
@@ -2,7 +2,11 @@
   <div class="fdz-funds">
     <FdzHeader />
     <FdzContentContainer>
-      <FdzVersion />
+      <FdzVersion class="fdz-funds__version" />
+      <div class="fdz-funds__controls">
+        <FdzIcon iconClass="fas fa-chevron-left" @click.native="onClick" />
+        <FdzIcon iconClass="fas fa-plus" />
+      </div>
     </FdzContentContainer>
   </div>
 </template>
@@ -10,6 +14,7 @@
 <script lang="ts">
 import FdzContentContainer from '../components/FdzContentContainer.vue'
 import FdzHeader from '../components/FdzHeader.vue'
+import FdzIcon from '../components/FdzIcon.vue'
 import FdzVersion from '../components/FdzVersion.vue'
 
 export default {
@@ -17,7 +22,13 @@ export default {
   components: {
     FdzContentContainer,
     FdzHeader,
+    FdzIcon,
     FdzVersion
+  },
+  methods: {
+    onClick: function () {
+      this.$router.push({ path: '/' })
+    }
   }
 }
 </script>
@@ -27,5 +38,35 @@ export default {
 
 .fdz-funds {
   height: 100%;
+
+  .fdz-funds__version + .fdz-funds__controls {
+    margin-top: 16px;
+  }
+
+  .fdz-funds__controls {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 16px;
+  }
+
+  .fdz-funds__grid {
+    display: grid;
+    column-gap: 16px;
+    row-gap: 16px;
+    margin-bottom: 32px;
+  }
+
+  @include breakpoint('small') {
+    .fdz-funds__grid {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @include breakpoint('medium-large') {
+    .fdz-funds__grid {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
 }
 </style>
