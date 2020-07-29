08aaf970ceea2465d45d54ff0f645856bfffb92d
Scott Turner <sturner1979@hotmail.com>
2020-07-29 12:56:43 +0100

    validation for add fund, added directive money for masking target input

--- src/components/FdzAddFundForm.vue
+++ src/components/FdzAddFundForm.vue
@@ -2,17 +2,20 @@
   <div class="fdz-add-fund-form">
     <form autocomplete="off">
     <p><strong>Add New Fund</strong></p>
-    <div
-      class="fdz-add-fund-form__row"
-      :class="{ 'valid' : true }">
-        <input type="text" name="fundName" placeholder="Name" maxlength="20" />
-        <FdzMessage v-bind:options="fundNameErrorMessageOptions" />
+    <div class="fdz-add-fund-form__row">
+      <input type="text" name="fundName" placeholder="Name" maxlength="20" v-model="fundName" v-on:keyup="isValidFundName" />
+      <FdzMessage v-if="formErrors.fundName" v-bind:options="fundNameErrorMessageOptions" />
     </div>
-    <div
-      class="fdz-add-fund-form__row"
-      :class="{ 'valid' : false }">
-        <input type="text" name="fundTarget" placeholder="Target" maxlength="12" />
-        <FdzMessage v-bind:options="fundTargetErrorMessageOptions" />
+    <div class="fdz-add-fund-form__row">
+      <input
+        type="text"
+        name="fundTarget"
+        placeholder="Target"
+        maxlength="12"
+        v-model="fundTarget"
+        v-on:keyup="isValidFundTarget"
+        v-money="moneyConfig" />
+      <FdzMessage v-if="formErrors.fundTarget" v-bind:options="fundTargetErrorMessageOptions" />
     </div>
     <div class="fdz-add-fund-form__row">
       TODO: choose colour
@@ -24,49 +27,76 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { store } from '../store/store'
-import { LsKeys } from '../enums/ls-keys.enum'
+import { VMoney } from 'v-money'
 import FdzButton from '../components/FdzButton.vue'
 import FdzMessage from '../components/FdzMessage.vue'
 import { FdzButtonModel } from '../models/fdz-button.model'
 import { FdzMessageModel } from '../models/fdz-message.model'
+import { FdzFundService } from '../services/fdz-fund.service'
+
+const fundService = new FdzFundService()
 
 @Component({
   components: {
     FdzButton,
     FdzMessage
+  },
+  directives: {
+    money: VMoney
   }
 })
 export default class FdzAddFundForm extends Vue {
+  formErrors = {
+    fundName: false,
+    fundTarget: false
+  };
+
+  fundName = '';
+  fundTarget = '';
+  moneyConfig = { thousands: ',', precision: 0 }
+
   fundNameErrorMessageOptions: FdzMessageModel = {
-    text: [
-      'Must be at least 2 characters long.'
-    ],
+    text: ['Must be at least 2 characters long.'],
     type: 'error'
   };
 
   fundTargetErrorMessageOptions: FdzMessageModel = {
-    text: [
-      'Must be at least 1 character long.',
-      'Must be number (whole numbers only)'
-    ],
+    text: ['Must be at least 1 character long.', 'Must be number (whole numbers only)'],
     type: 'error'
   };
 
   submitButtonOptions: FdzButtonModel = { text: 'Add', type: 'button' };
 
-  addFund () {
-    const token = Math.random().toString(36).substr(2)
-    store.funds.push({
-      id: token,
-      colour: { name: 'redSalsa', colour: '#F94144' },
-      current: 0,
-      name: `New Fund ${token}`,
-      target: 1000
-    })
-    if (localStorage) {
-      localStorage.setItem(LsKeys.Funds, JSON.stringify(store.funds))
+  addFund (): void {
+    if (this.isFormValid()) {
+      const rawfundTarget = parseInt(this.fundTarget.split(',').join(''), 10)
+      const token = Math.random().toString(36).substr(2)
+      fundService.addFund({
+        id: token,
+        colour: { name: 'redSalsa', colour: '#F94144' },
+        current: 0,
+        name: this.fundName,
+        target: rawfundTarget
+      }).then(() => {
+        // fund added
+        console.log('fund added')
+      }).catch(() => {
+        // TODO: add error message to ui
+        console.log('an error occurred')
+      })
     }
+  }
+
+  isFormValid (): boolean {
+    return Object.keys(this.formErrors).every(k => this.formErrors[k] === false)
+  }
+
+  isValidFundName (): void {
+    this.formErrors.fundName = !(this.fundName.length >= 2 && this.fundName.length <= 20)
+  }
+
+  isValidFundTarget (): void {
+    this.formErrors.fundTarget = !(this.fundTarget.length >= 1 && this.fundTarget.length <= 13)
   }
 }
 </script>
--- src/services/fdz-fund.service.ts
+++ src/services/fdz-fund.service.ts
@@ -7,6 +7,18 @@ export class FdzFundService {
     return store.funds.filter(f => f.id === id)[0]
   }
 
+  addFund (fund: FdzFundModel): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        store.funds.push(fund)
+        if (localStorage) {
+          localStorage.setItem(LsKeys.Funds, JSON.stringify(store.funds))
+        }
+        resolve()
+      }, 500)
+    })
+  }
+
   deleteFund (fund: FdzFundModel): Promise<void> {
     return new Promise((resolve) => {
       setTimeout(() => {
