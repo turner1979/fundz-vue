f1772345ae0b3c8539b48c4c9a3886741f33003e
Scott Turner <sturner1979@hotmail.com>
2020-07-30 10:28:30 +0100

    add fund form validation now uses rxweb/reactive-forms package. Removed fund target masking for now

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ReactiveFormConfig, ClientLibrary } from '@rxweb/reactive-forms'
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,12 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+ReactiveFormConfig.clientLib = ClientLibrary.Vue
+ReactiveFormConfig.set({
+  validationMessage: {
+    required: 'Field is required',
+    minLength: 'Field minLength not reached',
+    maxLength: 'Field maxLength not reached'
+  }
+})
