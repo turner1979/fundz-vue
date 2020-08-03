6c46f843a8126d7d9e72e48a466c4be0c4532950
Scott Turner <sturner1979@hotmail.com>
2020-08-03 13:04:36 +0100

    Added getFunds method to local storage service class

--- src/services/fdz-local-storage.service.ts
+++ src/services/fdz-local-storage.service.ts
@@ -2,11 +2,18 @@ import { LsKeys } from '../enums'
 import { FdzFundModel } from '../models'
 
 export class FdzLocalStorageService {
-  getFunds () {
-    // TODO
+  getFunds (): FdzFundModel[] {
+    const funds: FdzFundModel[] = []
+    if (localStorage && localStorage.getItem(LsKeys.Funds) !== null) {
+      const str = localStorage.getItem(LsKeys.Funds)
+      if (str) {
+        JSON.parse(str).map((fund: FdzFundModel) => funds.push(fund))
+      }
+    }
+    return funds
   }
 
-  saveFunds (funds: FdzFundModel[]) {
+  saveFunds (funds: FdzFundModel[]): void {
     if (localStorage) {
       localStorage.setItem(LsKeys.Funds, JSON.stringify(funds))
     }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,9 @@
 import Vue from 'vue'
-import { LsKeys } from '@/enums'
 import { FdzFundModel } from '@/models'
+import { FdzLocalStorageService } from '@/services'
 
 // Get funds data from localstorage
-const funds: FdzFundModel[] = []
-if (localStorage && localStorage.getItem(LsKeys.Funds) !== null) {
-  const str = localStorage.getItem(LsKeys.Funds)
-  if (str) {
-    JSON.parse(str).map((fund: FdzFundModel) => funds.push(fund))
-  }
-}
+const funds: FdzFundModel[] = new FdzLocalStorageService().getFunds()
 
-// Funds from local storage are set in simple store (vuex could be used for larger apps)
+// Funds are set in simple store (vuex could be used for larger apps)
 export const store = Vue.observable({ funds })
